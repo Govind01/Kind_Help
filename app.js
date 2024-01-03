b4a20e41c08b8a15bc3ddba577488d2ebacb309a
Govind <103506894+Govind01@users.noreply.github.com>
2024-01-03 18:19:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ LocalStrategy = require("passport-local");
 passportLocalMongoose = require("passport-local-mongoose");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/KindHelptDb");
+mongoose.connect("mongodb://127.0.0.1:27017");
 var db = mongoose.connection;
 db.on("error", console.log.bind(console, "connection error"));
 db.once("open", function (callback) {
